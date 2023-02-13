735691e4dbd5389da1611bf98bdadca9495e1e35
Iaroslav <GunGalla@gmail.com>
2023-02-13 19:59:39 +0200

    Add article model, make migrations

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class Article(models.Model):
+    name = models.CharField(max_length=200) # название статьи
+    body = models.TextField() # тело статьи
+    timestamp = models.DateTimeField(auto_now_add=True)
 # Create your models here.
