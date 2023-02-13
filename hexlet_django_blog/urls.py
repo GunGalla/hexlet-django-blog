72b4fb8d1ba62d64e59ee984692b099d068e5d37
Iaroslav <GunGalla@gmail.com>
2023-02-13 16:03:47 +0200

    Add clas as a view

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from hexlet_django_blog import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.Index.as_view()),
     path('about/', views.about),
     path('article/', include('hexlet_django_blog.article.urls')),
     path('admin/', admin.site.urls),
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
-def index(request):
-    return render(request, 'index.html', context={
-        'who': 'World',
-    })
+class Index(View):
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html', context={
+            'who': 'World',
+        })
 
 
 def about(request):
