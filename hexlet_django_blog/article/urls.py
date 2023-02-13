201142e4e67b2219de54d445e69a930ddfb2c460
Iaroslav <GunGalla@gmail.com>
2023-02-13 13:46:29 +0200

    Add template for article page

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [
-        path('', views.index),
+        path('', views.article),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('article')
+def article(request):
+    return render(request, 'article.html', context={
+        'article': 'List of articles',
+    })
