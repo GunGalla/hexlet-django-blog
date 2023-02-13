1a08fa8461a7f7780698c444f9bd618f4f90888f
Iaroslav <GunGalla@gmail.com>
2023-02-13 14:20:01 +0200

    Add base template

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -7,5 +7,5 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'about.html')
-
+    tags = ['обучение', 'программирование', 'python', 'oop']
+    return render(request, 'about.html', context={'tags': tags})
