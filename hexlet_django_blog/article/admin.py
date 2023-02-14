a1ee492204ee29bf9d82956da035912dc313e88d
Iaroslav <GunGalla@gmail.com>
2023-02-14 13:17:28 +0200

    Add superuser support

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
-# Register your models here.
+from .models import Article
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'timestamp')
+    search_fields = ['name', 'body']
+    list_filter = (('timestamp', DateFieldListFilter),)
+
+
--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -4,4 +4,6 @@ class Article(models.Model):
     name = models.CharField(max_length=200) # название статьи
     body = models.TextField() # тело статьи
     timestamp = models.DateTimeField(auto_now_add=True)
-# Create your models here.
+    
+    def __str__(self):
+        return self.name
