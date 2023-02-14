67e50499ad0763c40e02c7ebda6276ab0e45fbd4
Iaroslav <GunGalla@gmail.com>
2023-02-14 13:30:14 +0200

    Add articles page

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from hexlet_django_blog.article import views
+from hexlet_django_blog.article.views import IndexView
 
 urlpatterns = [
-        path('<str:tags>/<int:article_id>', views.article, name='article'),
+    path('', IndexView.as_view()),
+    path('<str:tags>/<int:article_id>', views.article, name='article'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.views import View
+
+from hexlet_django_blog.article.models import Article
+
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
+        return render(request, 'index_art.html', context={
+            'articles': articles,
+        })
+
 
 def article(request, tags, article_id):
     return render(request, 'article.html', context={
