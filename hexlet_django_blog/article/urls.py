93688b3d3afea5282b5bdd83b2d0470371439465
Iaroslav <GunGalla@gmail.com>
2023-02-15 13:21:39 +0200

    Add edit articles feature

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -5,6 +5,7 @@ from hexlet_django_blog.article.views import IndexView, ArticleView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='articles'),
+    path('<int:id>/edit/', views.ArticleFormEditView.as_view(), name='articles_update'),
     path('<int:id>/', ArticleView.as_view(), name='article'),
     path('create/', views.ArticleFormCreateView.as_view(), name='articles_create'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -60,3 +60,23 @@ class ArticleCommentFormView(View):
         form = ArticleCommentForm(request.POST)
         if form.is_valid():
             form.save()
+
+
+class ArticleFormEditView(View):
+
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(request, 'update.html', {'form': form, 'article_id':article_id})
+
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles')
+
+        return render(request, 'update.html', {'form': form, 'article_id':article_id})
